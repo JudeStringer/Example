b29cda8c6aad9da9afe7f1091cc03a0566aa8348
admin <admin@admins-MacBook-Air.local>
2024-06-27 11:23:00 -0700

    saving file

---
+++ sum_example.py
@@ -0,0 +1,13 @@
+# this is a comment. this function adds every number up untill it get the sum
+def sum(num):
+    resulte=0
+    for i in range(num):
+    result = result + num -i
+    print(result)
+
+sum(5)
+sum(10)
+   
+   
+    
+
